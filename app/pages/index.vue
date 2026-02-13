ee92eb28d8d26104998763ae2392551dcf13e89f
samchen <samchen@gmail.com>
2026-02-13 16:31:24 +0800

    初始化-更新scss功能

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -35,8 +35,16 @@ useSeoMeta({
     <NuxtLink :to="localePath('demo')">
       <el-button type="primary">to demo</el-button>
     </NuxtLink>
+    <div class="test">test</div>
+    <div class="test2"></div>
   </div>
 </template>
 <style lang="scss" scoped>
-//-
+.test {
+  @include fontClamp($color: red, $min: 10, $max: 200);
+  @extend %useBoxTrimBoth;
+}
+.test2 {
+  @include shape(100, 20, red, 5);
+}
 </style>
